19c081c87a2e026fbb95dabec1fad779081d47e4
Akash <akash9086236324@gmail.com>
2025-01-06 22:20:03 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -37,6 +37,8 @@ function App() {
   function filteredData(products, selected, query){
     let filteredProducts = products
 
+    console.log('This is a commit in main branch ')
+
     //Filtering input items 
     if(query){
       filteredProducts = filteredItems
